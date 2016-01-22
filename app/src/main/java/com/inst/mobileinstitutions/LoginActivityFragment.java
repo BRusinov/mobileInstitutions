ab4c5239f4b19962bbf3bec47ddc8a5215d5feaa
BRusinov <borislav.rusinov@gmail.com>
2016-01-22 11:11:01 +0200

    Fixed Facebook hash key, because OpenSSL gave me a wrong one. Now I generated it by code.

--- app/src/main/java/com/inst/mobileinstitutions/LoginActivityFragment.java
+++ app/src/main/java/com/inst/mobileinstitutions/LoginActivityFragment.java
@@ -1,8 +1,13 @@
 package com.inst.mobileinstitutions;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Base64;
+import android.content.pm.Signature;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +23,9 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -36,6 +44,21 @@ public class LoginActivityFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.inst.mobileinstitutions",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
         mCallbackManager = CallbackManager.Factory.create();
         tokenTracker=new AccessTokenTracker() {
             @Override
@@ -49,10 +72,11 @@ public class LoginActivityFragment extends Fragment {
                 textView.setText(displayMessage(profile1));
             }
         };
-
         tokenTracker.startTracking();
         profileTracker.startTracking();
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
