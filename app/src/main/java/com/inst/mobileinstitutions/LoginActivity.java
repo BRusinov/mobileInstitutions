3a7ea70f7ab6c9756d6cc2bece8094977068f0fc
BRusinov <borislav.rusinov@gmail.com>
2016-02-05 19:53:24 +0200

    Adding ForgottenPassword- working fully. Now we send email with new password.

--- app/src/main/java/com/inst/mobileinstitutions/LoginActivity.java
+++ app/src/main/java/com/inst/mobileinstitutions/LoginActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -38,8 +39,10 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 
 import android.text.method.HideReturnsTransformationMethod;
@@ -63,6 +66,15 @@ import com.inst.mobileinstitutions.API.APICredentials;
 import com.inst.mobileinstitutions.API.Models.User;
 import com.inst.mobileinstitutions.Complaints.List.ComplaintListActivity;
 import com.inst.mobileinstitutions.Forms.List.FormListActivity;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import rx.Subscriber;
 import rx.functions.Action1;
@@ -340,20 +352,8 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // PUSH notification will be implemented here
-                        EditText TO = email;
-                        Intent emailIntent= new Intent(Intent.ACTION_SEND);
-                        emailIntent.setData(Uri.parse("mailto:"));
-                        emailIntent.setType("text/plan");
-                        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO.toString());
-                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Забравена парола");
-                        emailIntent.putExtra(Intent.EXTRA_TEXT, generateRandomString());
-                        try {
-                            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-                            finish();
-                        }
-                        catch (android.content.ActivityNotFoundException ex) {
-                            Toast.makeText(LoginActivity.this, "Няма регистриран потребител с такъв email адрес!", Toast.LENGTH_SHORT).show();
-                        }
+                        String p=generateRandomString();
+                        sendMail(email.getText().toString(),"Забравена парола",p);
 
                     }
                 })
@@ -366,6 +366,67 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
                 .show();
     }
 
+    private Session createSessionObject() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        return Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("borislav.rusinov@gmail.com", "kingsizeasdf1234");
+            }
+        });
+    }
+
+    private Message createMessage(String email, String subject, String messageBody, Session session) throws MessagingException, UnsupportedEncodingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("podaisignal.herokuapp.com","ПодайСигнал"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+    private class SendMailTask extends AsyncTask<Message, Void, Void> {
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(LoginActivity.this, "Моля, изчакайте", "Изпращане на email...", true, false);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(Message... messages) {
+            try {
+                Transport.send(messages[0]);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    private void sendMail(String email, String subject, String messageBody) {
+        Session session = createSessionObject();
+
+        try {
+            Message message = createMessage(email, subject, messageBody, session);
+            new SendMailTask().execute(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
         return email.contains("@");
