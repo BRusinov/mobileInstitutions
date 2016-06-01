0f9a48a25434de29abbe6e78130926287b737de7
batman <vesselin.russinov@feratel.com>
2016-06-01 13:01:49 +0300

    Removing unnecessary functions, variables and imports. Most of them are generated by Android Studio and we don't need them.

--- app/src/main/java/com/inst/mobileinstitutions/LoginActivity.java
+++ app/src/main/java/com/inst/mobileinstitutions/LoginActivity.java
@@ -7,66 +7,32 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.Random;
-
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
-
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
-import com.facebook.login.widget.LoginButton;
-import com.facebook.FacebookCallback;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-
-import com.facebook.CallbackManager;
 import com.inst.mobileinstitutions.API.APICall;
 import com.inst.mobileinstitutions.API.APICredentials;
 import com.inst.mobileinstitutions.API.Models.User;
-import com.inst.mobileinstitutions.Complaints.List.ComplaintListActivity;
 import com.inst.mobileinstitutions.Forms.List.FormListActivity;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -79,23 +45,9 @@ import javax.mail.internet.MimeMessage;
 import rx.Subscriber;
 import rx.functions.Action1;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
+public class LoginActivity extends BaseMenuActivity {
     CheckBox mCbShowPwd;
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
 
-    // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -104,16 +56,14 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
     private static final String CHAR_LIST =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private static final int RANDOM_STRING_LENGTH = 10;
-//    private LoginButton loginButton;
-    public CallbackManager callbackManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -134,8 +84,6 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
                 attemptLogin();
             }
         });
-
-
         // RegisterActivity button
         Button mRegister= (Button) findViewById(R.id.register);
         mRegister.setOnClickListener(new OnClickListener() {
@@ -147,7 +95,6 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-
         // Checkbox for show/hide password
         mCbShowPwd = (CheckBox) findViewById(R.id.cbShowPwd);
         //mCbShowPwd.setText("Show password");
@@ -179,52 +126,6 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
-
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (ContextCompat.checkSelfPermission(LoginActivity.this, READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this, READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(LoginActivity.this, new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            ActivityCompat.requestPermissions(LoginActivity.this, new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-
-
-    /* Callback received when a permissions request has been completed.
-    */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
 
         // Reset errors.
@@ -237,17 +138,12 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
 
         boolean cancel = false;
         View focusView = null;
-
-
-
-
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError("Прекалено къса парола!");
             focusView = mPasswordView;
             cancel = true;
         }
-
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError("Задължително поле!");
@@ -259,18 +155,12 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
             cancel = true;
         }
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
             focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            APICall.signIn(email, password);
+        } else {APICall.signIn(email, password);
             showProgress(true);
             APICall.getResource("complaints").subscribe(new Subscriber() {
                 @Override
                 public void onCompleted() {
-                    ///startActivity(sendit Intent(LoginActivity.this, ComplaintListActivity.class));
                     APICall.getUserByEmail(APICredentials.getUsername()).subscribe(new Action1<User>() {
                         @Override
                         public void call(User user) {
@@ -285,7 +175,7 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
                 public void onError(Throwable e) {
                     APICall.signOut();
                     loginError();
-                    Log.w("pesho", e);
+//                    Log.w("pesho", e);
                     showProgress(false);
                 }
 
@@ -294,8 +184,6 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
             });
         }
     }
-
-
     public String generateRandomString(){
 
         StringBuffer randStr = new StringBuffer();
@@ -321,10 +209,6 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
     private void loginError(){
         Toast.makeText(this, "Грешна комбинация на име/парола", Toast.LENGTH_LONG).show();
     }
-
-
-
-
     //RegisterActivity button
 
     private void register(){
@@ -362,7 +246,7 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
 
         return Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("borislav.rusinov@gmail.com", "kingsizeasdf1234");
+                return new PasswordAuthentication("podaisignal123@gmail.com", "izprashtame");
             }
         });
     }
@@ -415,23 +299,11 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
         }
     }
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -453,64 +325,12 @@ public class LoginActivity extends BaseMenuActivity implements LoaderCallbacks<C
                 }
             });
         } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-    }
-
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
     }
 }
